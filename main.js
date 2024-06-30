f6a9621a1658ad40440b7e1185ab5d4bda906c75
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:59:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function Fahad() {
+    return "Asslam o Alikum";
+}
+var salam = Fahad();
+console.log(salam);
+function problem() {
+    var number = 5 + 5;
+    return number;
+}
+var call = problem();
+console.log(call);
---
+++ main.ts
@@ -0,0 +1,32 @@
+function Fahad(){
+    return "Asslam o Alikum"
+}
+let salam = Fahad();
+console.log(salam);
+
+function problem(){
+    let number = 5+5;
+    return number;
+}
+let call = problem();
+console.log(call);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
